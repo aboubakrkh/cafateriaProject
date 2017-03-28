ce6c22ad67dbbafb97c845a6a137a5d8bb2d5bfc
aboubakrkh <quimico.aboubakr@gmail.com>
2017-03-28 17:29:54 -0400

    deleted

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   resources :odrder_products
   resources :categories
+  resources :users do
+  resources :orders do
   resources :products
-  resources :orders
-  resources :users
+end
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

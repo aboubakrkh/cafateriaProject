5f9a954b1da4b13742dbc116fc20766bfb917092
aboubakrkh <quimico.aboubakr@gmail.com>
2017-03-28 17:31:47 -0400

    deleted

--- db/migrate/20170323170326_create_categories.rb
+++
@@ -1,9 +0,0 @@
-class CreateCategories < ActiveRecord::Migration[5.0]
-  def change
-    create_table :categories do |t|
-      t.references :product, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
